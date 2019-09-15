02817ec7224371c5fd97d34390d4f6c586892384
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-09-15 08:05:26 -0300

    implementado menu lateral porem precisa ser atualizado a versão

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,21 +1,70 @@
-//import React from 'react';
-import {createSwitchNavigator, createAppContainer} from 'react-navigation';
+import React from 'react';
+import {createSwitchNavigator, createDrawerNavigator} from 'react-navigation';
 import Agenda from './screens/Agenda';
 import Auth from './screens/Auth';
+import commonStyles from './commonStyles';
 
-const MainRoutes = {
+const menuRoutes = {
+    Today: {
+        name: 'Today',
+        screen: props => <Agenda title="Hoje" daysAhead={0} {...props} />,
+        navigationOptions: {
+            title: 'Hoje',
+        },
+    },
+    Tomorrow: {
+        name: 'Tomorrow',
+        screen: props => <Agenda title="Amanhã" daysAhead={1} {...props} />,
+        navigationOptions: {
+            title: 'Amanhã',
+        },
+    },
+    Week: {
+        name: 'Week',
+        screen: props => <Agenda title="Semana" daysAhead={7} {...props} />,
+        navigationOptions: {
+            title: 'Semana',
+        },
+    },
+    Month: {
+        name: 'Month',
+        screen: props => <Agenda title="Mês" daysAhead={30} {...props} />,
+        navigationOptions: {
+            title: 'Mês',
+        },
+    },
+};
+const menuConfig = {
+    initialRouteName: 'Today',
+
+    contentOptions: {
+        //- Estilos na exibição das opções do menu
+        labelStyle: {
+            fontFamily: commonStyles.fontFamily,
+            fontWeight: 'normal',
+            fontSize: 20,
+        },
+        //- Estilos na exibição da opção ativa.
+        activeLabelStyle: {
+            color: '#080',
+        },
+    },
+};
+const MenuNavigator = createDrawerNavigator(menuRoutes, menuConfig);
+
+const mainRoutes = {
     Auth: {
         name: 'Auth',
         screen: Auth,
     },
     Home: {
         name: 'Home',
-        screen: Agenda,
+        screen: MenuNavigator,
     },
 };
-
-const MainNavigator = createSwitchNavigator(MainRoutes, {
+const mainConfig = {
     initialRouteName: 'Auth',
-});
+};
 
-export default createAppContainer(MainNavigator);
+const MainNavigator = createSwitchNavigator(mainRoutes, mainConfig);
+export default MainNavigator;
